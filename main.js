7b500894d7757ba60e4153e5f2beeb1b91d3d2e6
Kua <68657995+katakuriko-a@users.noreply.github.com>
2021-11-03 12:56:27 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,51 @@
 new Vue({
   el: "#app",
   data: {
-    message: "HelloWorld!",
-    number: "0",
-    // titles: [],
-    // images: [],
-    items:[],
-    query: "",
+    items: [],
+    query: "鬼滅",
   },
   methods: {
-    increment() {
-      this.number = ++this.number;
-    },
-    decrement() {
-      this.number = --this.number;
-    },
-    reverse() {
-      this.message = this.message.split("").reverse().join("");
+    async search() {
+      const items = await this.getApi();
+      console.log(items);
+
+      const texts = items.map((item) => {
+        return `
+        <div class="items-wrapper">
+        <img class='w100' src='${item.image}' />
+        <h3 class='mb8'>${item.title}</h3>
+        <a class='book_button' href='${item.link}', target='_blank'>
+        詳しく
+        </a>
+
+          </div>
+        `;
+      });
+      const results = document.querySelector("#result");
+      results.innerHTML = texts.join("");
+
+
     },
     async getApi() {
+      // APIを叩く
       const response = await fetch(
         `https://www.googleapis.com/books/v1/volumes?q=${this.query}`
-
       );
+      // data = json
       const data = await response.json();
-      console.log(data.items);
-      this.items = data.items;
-      // for (const item of items) {
-      //   const image = item.volumeInfo.imageLinks.smallThumbnail;
-      //   this.images.push(image)
-      //   const title = item.volumeInfo.title;
-      //   this.titles.push(title);
-      // }
+
+      // itemsを一個一個取得
+      const items = data.items.map((item) => {
+        const vi = item.volumeInfo;
+        return {
+          title: vi.title,
+          description: vi.description,
+          link: vi.infoLink,
+          image: vi.imageLinks ? vi.imageLinks.smallThumbnail : "./img/noimage.gif",
+        };
+      });
+
+      return items;
     },
   },
 });
